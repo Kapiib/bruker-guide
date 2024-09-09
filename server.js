2f274577ba83af22831ceb67ab2ffb4b77f4070b
Kacper Baran <barankacper02@gmail.com>
2024-09-09 13:25:03 +0200

    nearly finished site 1 and 2

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/guide", (req, res) => {
+    res.render("guide");
+})
+
+app.listen(process.env.PORT);
