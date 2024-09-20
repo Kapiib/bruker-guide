d99b59972cac987d0554eb70914affeeff6732a5
Kacper Baran <barankacper02@gmail.com>
2024-09-20 15:22:23 +0200

    working login and register

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrpyt = require("bcrypt")
 require("dotenv").config();
 
 
@@ -8,8 +11,14 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+mongoose.connect("mongodb://127.0.0.1:27017/guide")
+.then(() => console.log("connected!"))
+.catch((error) => console.log("error", error));
+
+const saltRounds = 10;
+
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render("index");
 })
 
 app.get("/guide", (req, res) => {
@@ -24,16 +33,60 @@ app.get("/register", (req, res) => {
     res.render("register");
 })
 
+app.get("/dashboard", (req, res) => {
+    res.render("dashboard");
+})
+
 app.listen(process.env.PORT);
 
 app.post("/login", (req, res) => {
     console.log("Logging in", req.body);
     const {email, password} = req.body;
 
-    console.log(email)
+    User.findOne({email: email}).then((user) => {
+        console.log("results", user)
+
+        bcrpyt.compare(password, user.password).then((result) => {
+            console.log(result);
+            if(result) {
+                res.status(200).redirect("/dashboard");
+            } else {
+                res.status(500).json({message: "lalala"})
+            }
+        })
+
+    }).catch((error) => {
+        console.log("error", error)
+    })
 })
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
     console.log("Making an account", req.body);
 
+    const { email, password, Rpassword } = req.body;
+
+    if (password == Rpassword) {
+
+        bcrpyt.hash(password, saltRounds, async function(error, hash) {
+
+            const {mail, password, Rpassword} = req.body;
+            const newUser = new User({email: email, password: hash})
+            const result = await newUser.save();
+            console.log(result);
+    
+                if(result._id) {
+                res.redirect("/login");
+                }
+        })
+    } else {
+        res.status(500).json({message:"Passwords do not match"})
+    }
 })
+
+
+const userSchema = new Schema({
+    email: String,
+    password: String
+})
+
+const User = mongoose.model("user", userSchema);
